86b1f8825054f00305c95dcd522d9de30a3467dd
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-07-12 12:47:08 +0200

    Fix HBM2DLL with hibernate 5.2+
    see https://stackoverflow.com/questions/44924930

--- bootstrap-business/src/main/java/org/ligoj/bootstrap/core/dao/MySQL5InnoDBUtf8Dialect.java
+++ bootstrap-business/src/main/java/org/ligoj/bootstrap/core/dao/MySQL5InnoDBUtf8Dialect.java
@@ -1,6 +1,7 @@
 package org.ligoj.bootstrap.core.dao;
 
 import org.hibernate.dialect.MySQL55Dialect;
+import org.hibernate.engine.jdbc.env.spi.NameQualifierSupport;
 
 /**
  * MySQL dialect with enforced UTF-8 encoding.
@@ -10,6 +11,11 @@ public class MySQL5InnoDBUtf8Dialect extends MySQL55Dialect {
 	@Override
 	public String getTableTypeString() {
 		return super.getTableTypeString() + " DEFAULT CHARSET=utf8";
+	}
+
+	@Override
+	public NameQualifierSupport getNameQualifierSupport() {
+		return NameQualifierSupport.NONE;
 	}
 
 }
