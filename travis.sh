2516fd73568bdac6b1d88421f276c5c725cfa3ab
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-04-11 09:53:00 +0200

    Fix travis CI

--- travis.sh
+++ travis.sh
@@ -82,16 +82,6 @@ function fixBuildVersion {
   echo "Project Version: $PROJECT_VERSION"
 }
 
-#
-# Configure Maven settings and install some script utilities
-#
-function configureTravis {
-  mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v56 | tar zx --strip-components 1 -C ~/.local
-  source ~/.local/bin/install
-}
-configureTravis
-
 case "$TARGET" in
 
 BUILD)
