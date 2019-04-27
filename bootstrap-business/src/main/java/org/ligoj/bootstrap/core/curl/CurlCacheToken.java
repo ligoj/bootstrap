c7769c14622b7ddbdc3d98ad89eb4f37995c3a4e
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-04-27 20:14:58 +0200

    Remove deprecated CURL functions

--- bootstrap-business/src/main/java/org/ligoj/bootstrap/core/curl/CurlCacheToken.java
+++ bootstrap-business/src/main/java/org/ligoj/bootstrap/core/curl/CurlCacheToken.java
@@ -41,28 +41,6 @@ public class CurlCacheToken {
 	 * @param exceptionSupplier
 	 *            The exception used when the token cannot be retrieved.
 	 * @return The token value either from the cache, either from the fresh computed one.
-	 * @deprecated Use {@link #getTokenCache(String, UnaryOperator, int, Supplier)}
-	 */
-	@CacheResult(cacheName = "curl-tokens")
-	@Deprecated
-	public String getTokenCache(@CacheKey @NotNull final String key, final Function<String, String> function,
-			final int retries, final Supplier<? extends RuntimeException> exceptionSupplier) {
-		// First access to this function
-		return getTokenCache(key, function::apply, retries, exceptionSupplier);
-	}
-
-	/**
-	 * Return a cache token.
-	 *
-	 * @param key
-	 *            The cache key.
-	 * @param function
-	 *            The {@link Function} used to retrieve the token value when the cache fails.
-	 * @param retries
-	 *            The amount of retries until the provider returns a not <code>null</code> value.
-	 * @param exceptionSupplier
-	 *            The exception used when the token cannot be retrieved.
-	 * @return The token value either from the cache, either from the fresh computed one.
 	 */
 	@CacheResult(cacheName = "curl-tokens")
 	public String getTokenCache(@CacheKey @NotNull final String key, final UnaryOperator<String> function,
@@ -70,29 +48,6 @@ public class CurlCacheToken {
 		// First access to this function
 		return IntStream.range(0, retries).mapToObj(i -> function.apply(key)).filter(Objects::nonNull).findFirst()
 				.orElseThrow(exceptionSupplier);
-	}
-
-	/**
-	 * Return a synchronized cache token.
-	 *
-	 * @param synchronizeObject
-	 *            The object used to synchronize the access to the cache.
-	 * @param key
-	 *            The cache key.
-	 * @param function
-	 *            The {@link Function} used to retrieve the token value when the cache fails.
-	 * @param retries
-	 *            The amount of retries until the provider returns a not <code>null</code> value.
-	 * @param exceptionSupplier
-	 *            The exception used when the token cannot be retrieved.
-	 * @return The token value either from the cache, either from the fresh computed one.
-	 * @deprecated Use {@link #getTokenCache(String, UnaryOperator, int, Supplier)}
-	 */
-	@Deprecated
-	public String getTokenCache(@NotNull final Object synchronizeObject, @NotNull final String key,
-			final Function<String, String> function, final int retries,
-			final Supplier<? extends RuntimeException> exceptionSupplier) {
-		return getTokenCache(synchronizeObject, key, function::apply, retries, exceptionSupplier);
 	}
 
 	/**
