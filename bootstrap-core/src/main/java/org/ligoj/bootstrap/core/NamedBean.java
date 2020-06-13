b0b4580694f68404ba757ce64d9cf7a8fd09f0e6
Fabrice Daugan <fabrice.daugan@gmail.com>
2020-06-13 15:45:03 +0200

    Ignore unknown JSON attribute

--- bootstrap-core/src/main/java/org/ligoj/bootstrap/core/NamedBean.java
+++ bootstrap-core/src/main/java/org/ligoj/bootstrap/core/NamedBean.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,14 @@ import lombok.ToString;
 /**
  * A named bean
  * 
- * @param <K>
- *            the type of the identifier
+ * @param <K> the type of the identifier
  */
 @Getter
 @Setter
 @ToString(of = "name")
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NamedBean<K extends Serializable> implements INamableBean<K> {
 
 	/**
@@ -32,7 +34,7 @@ public class NamedBean<K extends Serializable> implements INamableBean<K> {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Identifier of this bean. 
+	 * Identifier of this bean.
 	 */
 	private K id;
 
@@ -42,12 +44,9 @@ public class NamedBean<K extends Serializable> implements INamableBean<K> {
 	/**
 	 * Copy a bean to another one.
 	 * 
-	 * @param <T>
-	 *            Bean type.
-	 * @param from
-	 *            the source bean.
-	 * @param to
-	 *            the target bean.
+	 * @param <T>  Bean type.
+	 * @param from the source bean.
+	 * @param to   the target bean.
 	 */
 	public static <T extends Serializable> void copy(final INamableBean<T> from, final INamableBean<T> to) {
 		to.setId(from.getId());
