83a181aabf52c3cd2ef847b7c0cb7e8256ea0de9
Fabrice Daugan <fabrice.daugan@gmail.com>
2023-05-07 09:30:14 +0200

    Log denied access

--- bootstrap-business/src/main/java/org/ligoj/bootstrap/core/resource/mapper/AccessDeniedExceptionMapper.java
+++ bootstrap-business/src/main/java/org/ligoj/bootstrap/core/resource/mapper/AccessDeniedExceptionMapper.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ligoj.bootstrap.core.resource.AbstractMapper;
 import org.springframework.security.access.AccessDeniedException;
 
@@ -15,10 +16,12 @@ import org.springframework.security.access.AccessDeniedException;
  * Handles access denied exceptions like {@link AccessDeniedException} to a JSON string.
  */
 @Provider
+@Slf4j
 public class AccessDeniedExceptionMapper extends AbstractMapper implements ExceptionMapper<AccessDeniedException> {
 
 	@Override
 	public Response toResponse(final AccessDeniedException exception) {
+		log.debug("Denied access for user", exception);
 		return toResponse(Status.FORBIDDEN, "security", null);
 	}
 
--- bootstrap-business/src/main/java/org/ligoj/bootstrap/core/resource/mapper/ForbiddenExceptionMapper.java
+++ bootstrap-business/src/main/java/org/ligoj/bootstrap/core/resource/mapper/ForbiddenExceptionMapper.java
@@ -9,16 +9,19 @@ import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ligoj.bootstrap.core.resource.AbstractMapper;
 
 /**
  * Handles access denied exceptions like {@link ForbiddenException} to a JSON string.
  */
 @Provider
+@Slf4j
 public class ForbiddenExceptionMapper extends AbstractMapper implements ExceptionMapper<ForbiddenException> {
 
 	@Override
 	public Response toResponse(final ForbiddenException exception) {
+		log.debug("Forbidden access for user", exception);
 		return toResponse(Status.FORBIDDEN, "security", null);
 	}
 
