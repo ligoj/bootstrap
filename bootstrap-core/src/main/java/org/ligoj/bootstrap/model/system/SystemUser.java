e3e1d4da8254965eca99dd77218f0f12a6e0da03
Fabrice Daugan <fabrice.daugan@gmail.com>
2023-09-26 23:34:29 +0200

    Lint Java

--- bootstrap-core/src/main/java/org/ligoj/bootstrap/model/system/SystemUser.java
+++ bootstrap-core/src/main/java/org/ligoj/bootstrap/model/system/SystemUser.java
@@ -37,10 +37,12 @@ public class SystemUser implements Serializable {
 	/**
 	 * Administrator role implicit criteria.
 	 */
-	public static final String IS_ADMIN = "(EXISTS(SELECT 1 FROM SystemRoleAssignment ra INNER JOIN ra.role r"
-			+ " WHERE ra.user.id = :user"
-			+ " AND EXISTS(SELECT 1 FROM SystemAuthorization a WHERE a.role = r AND a.pattern = '.*'"
-			+ " AND a.type = org.ligoj.bootstrap.model.system.SystemAuthorization$AuthorizationType.API)))";
+	public static final String IS_ADMIN = """
+			 (EXISTS(SELECT 1 FROM SystemRoleAssignment ra INNER JOIN ra.role r WHERE ra.user.id = :user
+			         AND EXISTS(SELECT 1 FROM SystemAuthorization a WHERE a.role = r AND a.pattern = '.*'
+			                AND a.type = org.ligoj.bootstrap.model.system.SystemAuthorization$AuthorizationType.API)
+			 ))
+			""";
 
 	/**
 	 * SID
