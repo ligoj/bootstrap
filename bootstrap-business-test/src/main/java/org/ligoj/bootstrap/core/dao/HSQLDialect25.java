8086464314827c4f7a76427317b0151eab1d54d0
Fabrice Daugan <fabrice.daugan@gmail.com>
2024-03-10 14:16:48 +0100

    Register ceil function for HSQLDB

--- bootstrap-business-test/src/main/java/org/ligoj/bootstrap/core/dao/HSQLDialect25.java
+++ bootstrap-business-test/src/main/java/org/ligoj/bootstrap/core/dao/HSQLDialect25.java
@@ -3,7 +3,11 @@
  */
 package org.ligoj.bootstrap.core.dao;
 
+import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.dialect.HSQLDialect;
+import org.hibernate.type.StandardBasicTypes;
+
+import static org.hibernate.query.sqm.produce.function.FunctionParameterType.NUMERIC;
 
 /**
  * HSQL dialect with keyword registration.
@@ -17,4 +21,19 @@ public class HSQLDialect25 extends HSQLDialect {
 		super();
 		registerKeyword("PERIOD");
 	}
+
+
+	@Override
+	public void initializeFunctionRegistry(FunctionContributions functionContributions) {
+		super.initializeFunctionRegistry(functionContributions);
+		registerFunctions(functionContributions);
+	}
+
+	private void registerFunctions(FunctionContributions functionContributions) {
+		functionContributions.getFunctionRegistry().namedDescriptorBuilder("ceil")
+				.setExactArgumentCount(1).setParameterTypes(NUMERIC)
+				.setInvariantType(functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.DOUBLE))
+				.register();
+	}
+
 }
