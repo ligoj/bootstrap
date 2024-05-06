d39a3441598043d3b5dd666d20294fa8edfe0875
Fabrice Daugan <fabrice.daugan@gmail.com>
2024-05-06 15:32:13 +0200

    Bump hibernate to v6.5.0.Final

--- bootstrap-core/src/main/java/org/ligoj/bootstrap/core/dao/ImplicitNamingStrategyNiceJpaImpl.java
+++ bootstrap-core/src/main/java/org/ligoj/bootstrap/core/dao/ImplicitNamingStrategyNiceJpaImpl.java
@@ -3,19 +3,18 @@
  */
 package org.ligoj.bootstrap.core.dao;
 
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
-import org.hibernate.boot.model.naming.Identifier;
-import org.hibernate.boot.model.naming.ImplicitAnyKeyColumnNameSource;
-import org.hibernate.boot.model.naming.ImplicitForeignKeyNameSource;
-import org.hibernate.boot.model.naming.ImplicitJoinColumnNameSource;
-import org.hibernate.boot.model.naming.ImplicitJoinTableNameSource;
-import org.hibernate.boot.model.naming.ImplicitUniqueKeyNameSource;
+import org.hibernate.HibernateException;
+import org.hibernate.boot.model.naming.*;
 import org.hibernate.boot.model.source.spi.AttributePath;
 import org.hibernate.mapping.Column;
-import org.hibernate.mapping.Constraint;
 import org.hibernate.mapping.Table;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Implements the original legacy naming behavior :
@@ -69,13 +68,40 @@ public class ImplicitNamingStrategyNiceJpaImpl
 
 	@Override
 	public Identifier determineForeignKeyName(final ImplicitForeignKeyNameSource source) {
-		return toIdentifier(Constraint.generateName("FK_", new Table(null, source.getTableName().getText()),
+		return toIdentifier(generateName("FK_", new Table(null, source.getTableName().getText()),
 				toColumns(source.getColumnNames())), source.getBuildingContext());
+	}
+
+	private static String generateName(String prefix, Table table, List<Column> columns) {
+		// Use a concatenation that guarantees uniqueness, even if identical names
+		// exist between all table and column identifiers.
+		final StringBuilder sb = new StringBuilder("table`" + table.getName() + "`");
+		// Ensure a consistent ordering of columns, regardless of the order they were bound.
+		// Clone the list, as sometimes a set of order-dependent Column bindings are given.
+		columns.stream()
+				.filter((Object thing) -> thing instanceof Column)
+				.sorted(Comparator.comparing(Column::getName))
+				.forEach(column -> sb.append("column`").append(column.getName()).append("`"));
+		return prefix + hashedName(sb.toString());
+	}
+
+	private static String hashedName(String name) {
+		try {
+			final MessageDigest md = MessageDigest.getInstance("MD5");
+			md.reset();
+			md.update(name.getBytes());
+			// By converting to base 35 (full alphanumeric), we guarantee
+			// that the length of the name will always be smaller than the 30
+			// character identifier restriction enforced by a few dialects.
+			return new BigInteger(1, md.digest()).toString(35);
+		} catch (NoSuchAlgorithmException e) {
+			throw new HibernateException("Unable to generate a hashed Constraint name", e);
+		}
 	}
 
 	/**
 	 * Return the column of given identifiers.
-	 * 
+	 *
 	 * @param identifiers The identifiers to map.
 	 * @return The column of given identifiers.
 	 */
@@ -85,7 +111,7 @@ public class ImplicitNamingStrategyNiceJpaImpl
 
 	@Override
 	public Identifier determineUniqueKeyName(final ImplicitUniqueKeyNameSource source) {
-		return toIdentifier(Constraint.generateName("UK_", new Table(source.getTableName().getText()),
+		return toIdentifier(generateName("UK_", new Table(source.getTableName().getText()),
 				toColumns(source.getColumnNames())), source.getBuildingContext());
 	}
 }
