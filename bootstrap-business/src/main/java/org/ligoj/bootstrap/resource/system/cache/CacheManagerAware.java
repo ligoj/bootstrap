17cb77057c02008807b6bf42d96d340bc4b16099
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-04-05 01:45:46 +0200

    feat: allow cache override for authorizations

--- bootstrap-business/src/main/java/org/ligoj/bootstrap/resource/system/cache/CacheManagerAware.java
+++ bootstrap-business/src/main/java/org/ligoj/bootstrap/resource/system/cache/CacheManagerAware.java
@@ -4,20 +4,35 @@
 package org.ligoj.bootstrap.resource.system.cache;
 
 import com.hazelcast.cache.HazelcastCacheManager;
+import com.hazelcast.config.CacheConfig;
+
+import java.util.function.Function;
 
 /**
  * Callback when cache manager is built but not yet injected in the beans.
  */
-@FunctionalInterface
 public interface CacheManagerAware {
 
 	/**
 	 * Callback when cache manager is built but not yet injected in the beans.
 	 *
-	 * @param cacheManager
-	 *            The cache manger backed by {@link HazelcastCacheManager}.
-	 * @param configurer
-	 *            The {@link CacheConfigurer} initializer accepting the cache name and default TTL as parameters.
+	 * @param cacheManager The cache manger backed by {@link HazelcastCacheManager}.
+	 * @param configurer   The {@link CacheConfigurer} initializer accepting the cache name and default TTL as parameters.
 	 */
-	void onCreate(HazelcastCacheManager cacheManager, CacheConfigurer configurer);
+	default void onCreate(HazelcastCacheManager cacheManager, CacheConfigurer configurer) {
+		onCreate(cacheManager, (name) -> configurer.newCacheConfig(name));
+	}
+
+	/**
+	 * Callback when cache manager is built but not yet injected in the beans.
+	 *
+	 * @param cacheManager The cache manger backed by {@link HazelcastCacheManager}.
+	 * @param provider
+	 *            The {@link CacheConfig} initializer accepting the cache name as {@link Function} parameter.
+	 */
+	@Deprecated
+	default void onCreate(final HazelcastCacheManager cacheManager, final Function<String, CacheConfig<?, ?>> provider) {
+		// Ignore
+	}
+
 }
